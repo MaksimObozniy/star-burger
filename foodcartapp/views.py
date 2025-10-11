b52c749ad770bbb0735ef0643c17d2dfb5a51b15
Maksim.Oboznyi <mobozn@gmail.com>
2025-10-11 19:45:11 +0400

    Extend data validation for def register_order

--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,11 +1,13 @@
 from django.http import JsonResponse
 from django.templatetags.static import static
-from django.shortcuts import get_object_or_404
+from django.core.exceptions import ObjectDoesNotExist
 from .models import Product, Order, OrderItem
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
+import phonenumbers
+
 
 def banners_list_api(request):
     # FIXME move data to db?
@@ -62,44 +64,109 @@ def product_list_api(request):
 def register_order(request):
 
     request_data = request.data
-    
+
+    #Проверка продуктов
     if 'products' not in request_data:
         return Response(
             {'products': 'Обязательное поле.'},
             status=status.HTTP_400_BAD_REQUEST
         )
-    
+
     products = request_data['products']
-    
+
     if products is None:
         return Response(
             {'products': 'Поле не может быть пустым.'},
             status=status.HTTP_400_BAD_REQUEST
         )
-    
+
     if not isinstance(products, list):
         return Response(
             {'products': f'Ожидался list со значениями, но был получен {type(products).__name__}.'},
             status=status.HTTP_400_BAD_REQUEST
         )
-    
+
     if len(products) == 0:
         return Response(
-            {'products': 'Список не может быть пустым.'},
+            {'products': 'Этот список не может быть пустым.'},
             status=status.HTTP_400_BAD_REQUEST
         )
     
+    #Проверка остальных полей
+
+    required_fields = ['firstname', 'lastname', 'phonenumber', 'address']
+
+    #Проверка на присутсвие значений в ключах
+    for field in required_fields:
+        if field not in request_data:
+            return Response(
+                {field : 'Обязательное поле.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    #Проверка полей на корректность заполнения
+    for field in required_fields:
+
+        field_value = request_data[field]
+
+        if field_value is None:
+            return Response(
+                {field: 'Это поле не может быть пустым.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if not isinstance(field_value, str):
+            return Response(
+                {field: 'Not a valid string.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+        if field_value.strip() == '':
+            return Response(
+                {field: 'Это поле не может быть пустым.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    #Проверка номера телефона
+    phonenumber_data = request_data['phonenumber']
+
+    try:
+        parsed_phone = phonenumbers.parse(phonenumber_data, 'RU')
+        if not phonenumbers.is_valid_number(parsed_phone):
+            return Response(
+                {'phonenumber': 'Введен некорректный номер телефона.'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    except phonenumbers.NumberParseException:
+        return Response(
+            {'phonenumber': 'Введен некорректный номер телефона.'},
+            status=status.HTTP_400_BAD_REQUEST
+        )
+
+    #Создание заказа
     order = Order.objects.create(
         firstname=request_data['firstname'],
         lastname=request_data['lastname'],
         phonenumber=request_data['phonenumber'],
         address=request_data['address'],
     )
-    for item in request_data['products']:
-        product = get_object_or_404(Product, id=item['product'])
+
+    for item in products:
+        try:
+            product = Product.objects.get(id=item['product'])
+        except (KeyError, ObjectDoesNotExist):
+            bad_key = item.get('product', None)
+            return Response(
+                {"products": f'Недопустимый первичный ключ "{bad_key}"'},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
         OrderItem.objects.create(
-            order=order, product=product,
+            order=order,
+            product=product,
             quantity=int(item['quantity']),
             price=product.price,
         )
+
     return Response({'status': 'ok', 'order_id': order.id})
