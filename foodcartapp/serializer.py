cd7f49be7ba8ba073f25acf2aa7de9f3f02302c9
Maksim.Oboznyi <mobozn@gmail.com>
2025-10-28 22:14:13 +0400

    Rewrite data validation logic with DRF.

---
+++ foodcartapp/serializer.py
@@ -0,0 +1,90 @@
+from rest_framework import serializers
+import phonenumbers
+
+from .models import Order, Product, OrderItem
+
+
+class OrderItemsListSerializer(serializers.ListSerializer):
+    def to_internal_value(self, data):
+        if data is None:
+            raise serializers.ValidationError('Это поле не может быть пустым.')
+        if not isinstance(data, list):
+            t = type(data).__name__
+            raise serializers.ValidationError(f'Ожидался лист со значениями, но был получен "{t}"')
+        if len(data) == 0:
+            raise serializers.ValidationError('Этот список не может быть пустым.')
+        return super().to_internal_value(data)
+
+
+class OrderItemInputSerializer(serializers.Serializer):
+    product = serializers.PrimaryKeyRelatedField(
+        queryset=Product.objects.all(),
+        error_messages={
+            'does_not_exist': 'Недоступный первичный ключ "{pk_value}"',
+            'incorrect_type': 'Недопустимый первичный ключ "{input}"',
+        },
+    )
+    quantity = serializers.IntegerField(min_value=1)
+
+    class Meta:
+        list_serializer_class = OrderItemsListSerializer
+
+class OrderCreateSerializer(serializers.Serializer):
+    firstname = serializers.CharField(
+        allow_blank=False, allow_null=False,
+        error_messages={
+            'blank': 'Это поле не может быть пустым',
+            'null': 'Это поле не может быть пустым',
+            'invalid': 'Not a valid string.'
+        },
+    )
+    lastname = serializers.CharField(
+        allow_blank=False, allow_null=False,
+        error_messages={
+            'blank': 'Это поле не может быть пустым.',
+            'null': 'Это поле не может быть пустым.',
+            'invalid': 'Not a valid string.',
+        },
+    )
+    phonenumber = serializers.CharField(
+        allow_blank=False, allow_null=False,
+        error_messages={
+            'blank': 'Это поле не может быть пустым.',
+            'null': 'Это поле не может быть пустым.',
+            'invalid': 'Not a valid string.',
+        },
+    )
+    address = serializers.CharField(
+        allow_blank=False, allow_null=False,
+        error_messages={
+            'blank': 'Это поле не может быть пустым.',
+            'null': 'Это поле не может быть пустым.',
+            'invalid': 'Not a valid string.',
+        },
+    )
+
+    products = OrderItemInputSerializer(many=True, allow_empty=False)
+
+
+    def validate_phonenumber(self, value: str):
+        try:
+            parsed = phonenumbers.parse(value, 'RU')
+            if not phonenumbers.is_valid_number(parsed):
+                raise serializers.ValidationError('Введен некорректный номер телефона.')
+        except phonenumbers.NumberParseException:
+            raise serializers.ValidationError('Введен некорректный номер телефона.')
+        return value
+
+    def create(self, validated_data):
+        items = validated_data.pop('products')
+        order = Order.objects.create(**validated_data)
+
+        for item in items:
+            product = item['product']
+            OrderItem.objects.create(
+                order=order,
+                product=product,
+                quantity=item['quantity'],
+                price=product.price,
+            )
+        return order
--- foodcartapp/views.py
+++ foodcartapp/views.py
@@ -1,13 +1,11 @@
 from django.http import JsonResponse
 from django.templatetags.static import static
-from django.core.exceptions import ObjectDoesNotExist
 from .models import Product, Order, OrderItem
 
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-import phonenumbers
-
+from .serializer import OrderCreateSerializer
 
 def banners_list_api(request):
     # FIXME move data to db?
@@ -62,111 +60,7 @@ def product_list_api(request):
 
 @api_view(['POST'])
 def register_order(request):
-
-    request_data = request.data
-
-    #Проверка продуктов
-    if 'products' not in request_data:
-        return Response(
-            {'products': 'Обязательное поле.'},
-            status=status.HTTP_400_BAD_REQUEST
-        )
-
-    products = request_data['products']
-
-    if products is None:
-        return Response(
-            {'products': 'Поле не может быть пустым.'},
-            status=status.HTTP_400_BAD_REQUEST
-        )
-
-    if not isinstance(products, list):
-        return Response(
-            {'products': f'Ожидался list со значениями, но был получен {type(products).__name__}.'},
-            status=status.HTTP_400_BAD_REQUEST
-        )
-
-    if len(products) == 0:
-        return Response(
-            {'products': 'Этот список не может быть пустым.'},
-            status=status.HTTP_400_BAD_REQUEST
-        )
-    
-    #Проверка остальных полей
-
-    required_fields = ['firstname', 'lastname', 'phonenumber', 'address']
-
-    #Проверка на присутсвие значений в ключах
-    for field in required_fields:
-        if field not in request_data:
-            return Response(
-                {field : 'Обязательное поле.'},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
-    #Проверка полей на корректность заполнения
-    for field in required_fields:
-
-        field_value = request_data[field]
-
-        if field_value is None:
-            return Response(
-                {field: 'Это поле не может быть пустым.'},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
-        if not isinstance(field_value, str):
-            return Response(
-                {field: 'Not a valid string.'},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
-        if field_value.strip() == '':
-            return Response(
-                {field: 'Это поле не может быть пустым.'},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
-    #Проверка номера телефона
-    phonenumber_data = request_data['phonenumber']
-
-    try:
-        parsed_phone = phonenumbers.parse(phonenumber_data, 'RU')
-        if not phonenumbers.is_valid_number(parsed_phone):
-            return Response(
-                {'phonenumber': 'Введен некорректный номер телефона.'},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
-    except phonenumbers.NumberParseException:
-        return Response(
-            {'phonenumber': 'Введен некорректный номер телефона.'},
-            status=status.HTTP_400_BAD_REQUEST
-        )
-
-    #Создание заказа
-    order = Order.objects.create(
-        firstname=request_data['firstname'],
-        lastname=request_data['lastname'],
-        phonenumber=request_data['phonenumber'],
-        address=request_data['address'],
-    )
-
-    for item in products:
-        try:
-            product = Product.objects.get(id=item['product'])
-        except (KeyError, ObjectDoesNotExist):
-            bad_key = item.get('product', None)
-            return Response(
-                {"products": f'Недопустимый первичный ключ "{bad_key}"'},
-                status=status.HTTP_400_BAD_REQUEST
-            )
-
-        OrderItem.objects.create(
-            order=order,
-            product=product,
-            quantity=int(item['quantity']),
-            price=product.price,
-        )
-
-    return Response({'status': 'ok', 'order_id': order.id})
+    serializer = OrderCreateSerializer(data=request.data)
+    serializer.is_valid(raise_exception=True)
+    order = serializer.save()
+    return Response({'status': 'ok', 'order_id': order.id}, status=status.HTTP_200_OK)
