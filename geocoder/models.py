6fb3a385d86ec39abb1fff97ef67b3addac472ef
MaksimObozniy <125642497+MaksimObozniy@users.noreply.github.com>
2025-12-18 14:53:59 +0100

    update geocoder models.py

--- geocoder/models.py
+++ geocoder/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class Place(models.Model):
-    address = models.CharField(max_length=255, unique=True)
-    latitude = models.FloatField(null=True, blank=True)
-    longitude = models.FloatField(null=True, blank=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    address = models.CharField(max_length=255, unique=True, verbose_name='Адресс')
+    latitude = models.FloatField(null=True, blank=True, verbose_name="Ширина")
+    longitude = models.FloatField(null=True, blank=True, verbose_name="Долгота")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Последнее обновление")
     
     def __str__(self):
         return self.query
