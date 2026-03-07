b2940888467c2e5ee76e9f17278a68a95fb3fe89
Maksim Oboznyi <mobozn@gmail.com>
2026-03-07 20:13:56 +0000

    update project

--- bundles-src/index.jsx
+++ bundles-src/index.jsx
@@ -1,17 +1,15 @@
 import "core-js/stable";
 import "regenerator-runtime/runtime";
 
-import { createRoot } from 'react-dom/client';
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import './index.css';
 import App from './App';
 import './css/products.css';
 import './css/product.css';
 
-const container = document.getElementById('root');
-const root = createRoot(container);
-root.render(<App />);
+ReactDOM.render(<App />, document.getElementById('root'));
 
 // Workaround for Parcel bug https://github.com/parcel-bundler/parcel/issues/2894
 if (module.hot) {
